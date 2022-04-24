7776f9ab06bccb2fffd89096ccfdc124f04635a0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-24 19:25:38 +0800

    Delete server directory

--- server/service.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: "google"
-});
-ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: "959537698298-o6m1fvqvt717maifjp196bemi3ku00hu.apps.googleusercontent.com",
-  loginStyle: "popup",
-  secret: "vfbVr4zNmQFTAARGBq2jiPeX"
-});
